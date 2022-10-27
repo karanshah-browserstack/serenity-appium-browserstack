a97458409fc7d8ba5c3f3381065813554fe24495
Karan Shah <64479353+karanshah-browserstack@users.noreply.github.com>
2022-10-27 17:22:44 +0530

    SDK parallelism m*n

--- src/test/java/integration/browserstack/BrowserStackSerenityDriver.java
+++ src/test/java/integration/browserstack/BrowserStackSerenityDriver.java
@@ -1,56 +1,36 @@
 package integration.browserstack;
 
 import io.appium.java_client.android.AndroidDriver;
-import net.thucydides.core.environment.SystemEnvironmentVariables;
-import net.thucydides.core.util.EnvironmentVariables;
 
 import net.thucydides.core.webdriver.DriverSource;
 import org.openqa.selenium.MutableCapabilities;
+import org.yaml.snakeyaml.Yaml;
 
+import java.io.File;
+import java.io.InputStream;
 import java.net.URL;
+import java.nio.file.Files;
 import java.util.HashMap;
-import java.util.Iterator;
+import java.util.Map;
 
 public class BrowserStackSerenityDriver implements DriverSource {
+    public static String userName, accessKey;
+    public static Map<String, Object> browserStackYamlMap;
+    public static final String USER_DIR = "user.dir";
 
     @Override
     public AndroidDriver newDriver() {
-        EnvironmentVariables environmentVariables = SystemEnvironmentVariables.createEnvironmentVariables();
+        File file = new File(getUserDir() + "/browserstack.yml");
+        browserStackYamlMap = convertYamlFileToMap(file, new HashMap<>());
+        userName = System.getenv("BROWSERSTACK_USERNAME") != null ? System.getenv("BROWSERSTACK_USERNAME") : (String) browserStackYamlMap.get("userName");
+        accessKey = System.getenv("BROWSERSTACK_ACCESS_KEY") != null ? System.getenv("BROWSERSTACK_ACCESS_KEY") : (String) browserStackYamlMap.get("accessKey");
 
-
-        String username = GetInfoBrowserStack.getName();
-
-        String accessKey = GetInfoBrowserStack.getKey();
-        String environment = System.getProperty("environment");
-        System.out.println("environment is " + environment);
         MutableCapabilities capabilities = new MutableCapabilities();
         HashMap<String, Object> bStackOptionsMap = new HashMap<>();
-        Iterator it = environmentVariables.getKeys().iterator();
-        while (it.hasNext()) {
-            String key = (String) it.next();
-
-            if (key.equals("userName") || key.equals("accessKey")
-                    || key.equals("browserstack.server")) {
-                continue;
-            } else if (key.startsWith("bstack_")) {
-                if (key.equalsIgnoreCase("bstack_app")) {
-                    capabilities.setCapability(key.replace("bstack_", ""), environmentVariables.getProperty(key));
-                } else {
-                    bStackOptionsMap.put(key.replace("bstack_", ""), environmentVariables.getProperty(key));
-                }
-            } else if (environment != null && key.startsWith("environment." + environment)) {
-                if (key.equalsIgnoreCase("environment." + environment + ".app")) {
-                    capabilities.setCapability(key.replace("environment." + environment + ".", ""),
-                            environmentVariables.getProperty(key));
-                } else {
-                    bStackOptionsMap.put(key.replace("environment." + environment + ".", ""), environmentVariables.getProperty(key));
-                }
-                capabilities.setCapability(key.replace("environment." + environment + ".", ""),
-                        environmentVariables.getProperty(key));
-            }
-        }
+        bStackOptionsMap.put("source", "serenity-appium:run-parallel-features-sdk:v1.0");
+        capabilities.setCapability("bstack:options", bStackOptionsMap);
         try {
-            AndroidDriver driver = new AndroidDriver(new URL("https://" + username + ":" + accessKey + "@hub-cloud.browserstack.com/wd/hub"), capabilities);
+            AndroidDriver driver = new AndroidDriver(new URL("https://" + userName + ":" + accessKey + "@hub-cloud.browserstack.com/wd/hub"), capabilities);
             return driver;
 
         } catch (Exception e) {
@@ -62,6 +42,22 @@ public class BrowserStackSerenityDriver implements DriverSource {
     @Override
     public boolean takesScreenshots() {
         return true;
+    }
+
+    private String getUserDir() {
+        return System.getProperty(USER_DIR);
+    }
+
+    private Map<String, Object> convertYamlFileToMap(File yamlFile, Map<String, Object> map) {
+        try {
+            InputStream inputStream = Files.newInputStream(yamlFile.toPath());
+            Yaml yaml = new Yaml();
+            Map<String, Object> config = yaml.load(inputStream);
+            map.putAll(config);
+        } catch (Exception e) {
+            throw new RuntimeException(String.format("Malformed browserstack.yml file - %s.", e));
+        }
+        return map;
     }
 
 }
